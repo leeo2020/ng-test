ffd67617f13efd07c45c301aa3565f511eec12bf
leeo2020 <3145875098@qq.com>
2021-03-26 00:53:41 +0800

    angular 初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewsComponent } from './components/news/news.component';
+import { HomeComponent } from './components/home/home.component';
+import { GoodsComponent } from './components/goods/goods.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    HomeComponent,
+    GoodsComponent
   ],
   imports: [
     BrowserModule,
